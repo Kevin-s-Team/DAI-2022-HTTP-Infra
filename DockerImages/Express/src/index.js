83d85e499be2556ec7719b962ef57b549a745997
celestinPiccin <100125222+celestinPiccin@users.noreply.github.com>
2022-12-11 17:29:45 +0100

    step 2 done

---
+++ DockerImages/Express/src/index.js
@@ -0,0 +1,35 @@
+const Chance = require('chance');
+const chance = new Chance();
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send(getWeather());
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+
+function getWeather(){
+    var numberOfCities = chance.integer({
+        min: 1,
+        max: 20
+    });
+    var day = chance.weekday();
+
+    var cities = [];
+    for(var i = 0; i < numberOfCities; i++){
+        cities.push({
+            city: chance.city(),
+            country: chance.country({full: true}),
+            altitude: chance.altitude({fixed: 2, max: 3000}),
+            weekday: day,
+            degree: chance.floating({fixed: 2, min:-30, max:50})
+        });
+    };
+    return cities;
+}
--- README.md
+++ README.md
@@ -15,3 +15,17 @@ Les fichiers de configuration apache se trouvent dans le dossier /etc/apache2
 Pour accéder au container : ```docker exec -it <nom container> /bin/bash```
 
 Ensuite on peut naviguer dans l'arborescence du système en utilisant la commande cd
+
+## Step 2: Dynamic HTTP server with express.js
+
+Notre container qui utilise express.js renvoie la météo de différentes villes au client sous format JSON.
+
+Le JSON contient différentes informations, notamment le nom de la ville, le jour et la temperature.
+
+Les commandes suivantes ont été utiles:
+
+```npm init``` => pour créer une nouvelle application node.js
+
+```npm install --save chance``` => pour ajouter le module chance à l'application
+
+```npm install --save express``` => pour ajouter le module express.js à l'application
