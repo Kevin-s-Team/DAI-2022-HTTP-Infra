efcdcd1d38c1429272d9337070f590fb9c32a51e
KJD <25977492+kjorand@users.noreply.github.com>
2023-01-10 19:57:08 +0100

    No need no more with the new docker-compose

--- DockerImages/buildAllAndRunScale.ps1
+++ DockerImages/buildAllAndRunScale.ps1
@@ -1,7 +1,2 @@
 param([int] $dynamicScale = 2, [int] $staticScale = 2)
-cd .\ApachePHP
-.\build.ps1
-cd ..\Express
-.\build.ps1
-cd ..
 .\runScale.ps1 -dynamicScale $dynamicScale -staticScale $staticScale
